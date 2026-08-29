55ce19fa48e03069d114dbb3ec0ad4e438bf92d2
David Turrión Jurado <ideivoidi08@gmail.com>
2026-08-29 22:15:08 +0200

    Enhance player.py with function comments
    Added function documentation and improved code readability.

--- logic/player.py
+++ logic/player.py
@@ -2,6 +2,12 @@
 # Creacion del jugador
 #============================================================
 
+# | FUNCIONES |
+# Preguntar_player → Pregunta al jugador su nombre y clase
+# Pregunta_clase → Pregunta al jugador su clase y valida la respuesta
+# Info_clase → Muestra la informacion de la clase seleccionada |GUI|
+# Crear_personaje → Crea el objeto jugador con su nombre, clase y estadisticas base
+
 from text.textos import cargar_textos
 TEXTOS = cargar_textos()
 
@@ -27,6 +33,7 @@ def preguntar_player():
         if confirmar in ("si", "sí", "s"):
             return nombre, clase
 
+# AUXILIAR pregunta para la clase del jugador y valida la respuesta
 def pregunta_clase(texto_pregunta, texto_fallo):
     while True:
         print(texto_pregunta)
@@ -37,12 +44,12 @@ def pregunta_clase(texto_pregunta, texto_fallo):
         else:
             print(texto_fallo)
         
-
+# Muestra la informacion de la clase seleccionada |GUI|
 def info_clase(clase):
     print(TEXTOS["clases_info"][clase])
     #! ESTO MANDARA A LA GUI A PONER EN PANTALLA EL TEXTO Y A MOSTRAR SUS STATS
     
-
+# Crea el objeto jugador con su nombre, clase y estadisticas base
 def crear_personaje(nombre, clase):
     base = variar(JUGADOR[clase])
     return Entidad(nombre, True, clase, base)
